dbffb27278a291f2bbb3c79c0a61ac40e14a2d15
sherouk22 <sheroukmohy558@gmail.com>
2025-01-16 19:57:38 +0200

    Add project files.

---
+++ Task01OOp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task01OOp
+{
+    internal class Program
+    {//Task01OOp
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
